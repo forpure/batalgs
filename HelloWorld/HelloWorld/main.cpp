52acbbdb38c1673d97aea9a227c11ca1cf51a960
forpure <442853852@qq.com>
2018-01-11 00:26:08 +0800

    1st commit.

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Fan Zhang on 2018/1/11.
+//  Copyright © 2018年 Fan Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
